733074daff5eefe538c0aaea7423dd4785b73c3a
nyg <nyg@github>
2013-04-13 16:28:09 +0200

    Added files.

---
+++ bitstamp.js
@@ -0,0 +1,28 @@
+var previousTicker = { high: 0, last: 0, bid: 0, volume: 0, low: 0, ask: 0 };
+
+setInterval(function () {
+  
+  $.getJSON('https://www.bitstamp.net/api/ticker', function (ticker) {
+    
+    // loop through all object's attribute
+    for (key in ticker) {
+      console.update
+      // update HTML table
+      $('#' + key).text(ticker[key]);
+      
+      // update color
+      if (ticker[key] > previousTicker[key]) {
+        $('#' + key).css('color', 'green');
+      }
+      else if (ticker[key] < previousTicker[key]) {
+        $('#' + key).css('color', 'red');
+      }
+      else {
+        $('#' + key).css('color', 'black');
+      }
+    }
+    
+    previousTicker = ticker;
+  });
+  
+}, 2442);
