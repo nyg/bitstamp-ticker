b634135e6b4f4ff0be93ab5389f9fcdf7dbebe23
nyg <nyg@github>
2015-11-11 21:53:30 +0100

    - YQL apparently doesn't accept "?" as the callback value anymore, replaced $.getJSON by $.ajax. - Updated jQuery to version 2.1.4.

--- bitstamp.js
+++ bitstamp.js
@@ -1,33 +1,42 @@
-var previousTicker = { high: 0, last: 0, bid: 0, volume: 0, low: 0, ask: 0 },
-    api_url = 'https://www.bitstamp.net/api/ticker/'
-    yql_url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D"' + api_url + '"&format=json&callback=?';
+var previousTicker = { high: 0, last: 0, bid: 0, volume: 0, low: 0, ask: 0 };
 
 setInterval(function () {
-  
-  $.getJSON(yql_url, function (jsonp) {
-    
-    // parse response from YQL
-    var ticker = $.parseJSON(jsonp.query.results.body.p);
-    
-    // loop through all object's attribute
-    for (key in ticker) {
-      
-      // update HTML table
-      $('#' + key).text(ticker[key]);
-      
-      // update text color
-      if (ticker[key] > previousTicker[key]) {
-        $('#' + key).css('color', 'green');
+
+  $.ajax({
+
+    url: 'http://query.yahooapis.com/v1/public/yql',
+    jsonp: 'callback',
+    dataType: 'jsonp',
+
+    data: {
+      q: 'select * from html where url = "https://www.bitstamp.net/api/ticker/"',
+      format: 'json'
+    },
+
+    success: function(response) {
+
+      // parse response from YQL
+      var ticker = $.parseJSON(response.query.results.body);
+
+      // loop through all the ticker's attributes
+      for (key in ticker) {
+
+        // update HTML table
+        $('#' + key).text(ticker[key]);
+
+        // update text color
+        if (ticker[key] > previousTicker[key]) {
+          $('#' + key).css('color', 'green');
+        }
+        else if (ticker[key] < previousTicker[key]) {
+          $('#' + key).css('color', 'red');
+        }
+        else {
+          $('#' + key).css('color', 'black');
+        }
       }
-      else if (ticker[key] < previousTicker[key]) {
-        $('#' + key).css('color', 'red');
-      }
-      else {
-        $('#' + key).css('color', 'black');
-      }
+
+      previousTicker = ticker;
     }
-    
-    previousTicker = ticker;
   });
-  
-}, 2542);
+}, 1000);
