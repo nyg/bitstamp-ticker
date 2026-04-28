9ab5a0c89f2b1616398ab597405e7ebcda4f9b5a
nyg <nyg@users.noreply.github.com>
2026-04-28 23:52:00 +0200

    Enhance ticker update with currency formatting
    Refactor ticker update logic for improved readability and functionality.

--- bitstamp.js
+++ bitstamp.js
@@ -5,35 +5,31 @@ setInterval(getTicker, 1000)
 function getTicker() {
     fetch('https://www.bitstamp.net/api/ticker/')
         .then(response => {
-            if (!response.ok) throw new Error(response.status)
+            if (!response.ok) {
+                throw new Error(response.status)
+            }
             return response.json()
         })
         .then(ticker => {
-
             for (key in ticker) {
-
-                var element = document.getElementById(key)
+                const element = document.getElementById(key)
                 if (element == null) {
                     continue
                 }
 
                 // update HTML table
                 const amount = parseFloat(ticker[key])
-                if (key === 'volume') {
-                    element.textContent = '₿ ' + new Intl.NumberFormat(navigator.language, { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(amount)
-                }
-                else {
-                    element.textContent = new Intl.NumberFormat(navigator.language, { style: 'currency', currency: 'USD', currencyDisplay: 'narrowSymbol' }).format(amount)
-                }
-
+                const currency = key === 'volume' ? 'BTC'
+                element.textContent = new Intl.NumberFormat(navigator.language, { style: 'currency', currency, currencyDisplay: 'narrowSymbol' })
+                    .format(amount)
+                    .replace('BTC', '₿')
+                
                 // update text color
                 if (ticker[key] > previousTicker[key]) {
                     element.style.color = 'green'
-                }
-                else if (ticker[key] < previousTicker[key]) {
+                } else if (ticker[key] < previousTicker[key]) {
                     element.style.color = 'red'
-                }
-                else {
+                } else {
                     element.style.color = 'black'
                 }
             }
